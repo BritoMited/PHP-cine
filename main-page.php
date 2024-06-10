7074c4a12b369bd2061bd08e8d361f1e11559bc7
Auto Logon <autologon@CSED.LAB>
2024-06-10 09:48:59 -0300

    inicio main page e teste merge

--- main-page.php
+++ main-page.php
@@ -6,6 +6,18 @@
     <title>Home</title>
 </head>
 <body>
-    
+    <div> 
+        <head>
+            <label>Pesquisa</label>
+            <input type="text" name="" id="">
+            <a href="">home</a>
+            <a href="">login</a>
+            <a href="">home</a>
+        </head>
+        <br>
+        <div>
+            <p>*Lista Filmes*</p>
+        </div>
+    </div>
 </body>
 </html>
